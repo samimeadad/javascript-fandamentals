8f2df3bcd605bf2e49b1a959f3f9639a2b308a76
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-01 18:11:22 +0600

    partially done

---
+++ array-index.js
@@ -0,0 +1,15 @@
+var lovingPerson = ['sami', 'farmee', 'arishamma', 'ammapakhi'];
+var indexNo = lovingPerson.indexOf('ammapakhi');
+var number = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
+var searchItem = 75;
+var index = number.indexOf(searchItem);
+var ammapakhi = lovingPerson[3];
+
+// if(index == -1)
+//     console.log('Sorry! Search Item', + searchItem + ' is Not found');
+// else
+//     console.log('Congrats! You have found the number in array position ',index);
+
+console.log(indexNo);
+console.log(index);
+console.log(ammapakhi);
