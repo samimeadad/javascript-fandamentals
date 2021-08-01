8bed12901e4d82d26add562dd619eba8d9dd1759
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-01 16:07:02 +0600

    array added

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendsName = ['Oziar', 'Hasan', 'Rasel', 'Tanvir', 'Rubel', 'Rajib', 'Jakir'];
+var friendsAge = [40, 42, 39, 38, 40, 41, 40];
+
+console.log(friendsName);
+console.log(friendsAge);
+
+var friendsCount = friendsName.length;
+console.log('No. of Friends: ', friendsCount);
